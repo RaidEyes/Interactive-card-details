30b32706aa50f8c25ddb8b03b4fd87ff05766856
RaidEyes <phamquanghung199713@gmail.com>
2023-10-17 23:20:01 +0700

    Initialize HTML and basic CSS, No JS

---
+++ script.js
@@ -0,0 +1,17 @@
+const errorMessage = document.querySelectorAll(".error");
+const userInput = document.querySelectorAll(".card-input");
+
+userInput.forEach((input) => {
+  input.addEventListener("input", () => {
+    if (!input.validity.valid) {
+      errorMessage.forEach((message) => {
+        message.innerHTML = "Wrong type";
+      });
+    } else {
+      errorMessage.forEach((message) => {
+        message.innerHTML = "";
+      });
+      console.log(input.validity.valid);
+    }
+  });
+});
