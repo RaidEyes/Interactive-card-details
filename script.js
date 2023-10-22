3955791f8a402c3f5dd1334e3bf72011e5df8310
Trang Lê <lethithutrang380@gmail.com>
2023-10-22 20:47:05 +0700

    fix: submit form after show sucess

--- script.js
+++ script.js
@@ -118,3 +118,13 @@ creditNumber.addEventListener("input", (event) => {
 
 //todo1 Make complete message appear after inputing all the information
 //todo2 Store user input data to make sure they have already input their information
+
+const btnSubmitForm = document.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const cardFormEle = document.querySelector(".card-form");
+  cardFormEle.style.display = "none";
+
+  const submitSucessEle = document.querySelector(".wrap__submitsucess");
+  submitSucessEle.classList.remove("d-none");
+  submitSucessEle.classList.add("wrap__submitsucess");
+});
